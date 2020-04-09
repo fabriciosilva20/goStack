886f921b286ca3985568bfa56f9e9bd697f00fc2
Fabricio Moreira da Silva <fabriciosilva20@gmail.com>
2020-04-09 10:45:58 -0300

    desafio01

---
+++ backend/src/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const { uuid } = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+function logRequest(request, response, next) {
+  const {method, url} = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+  // sempre é preciso colocar o next() para prosseguir a aplicação 
+  // mmiddlewares é executado antes de chamar qq funcão neste caso.
+  return next();
+
+}
+///aplica aqui para ser usado em toda aplicacao
+//app.use(logRequest);
+
+//o middleware pode ser aplicado em uma funcao específica
+// e pode colcoar quantso quiser app.get('/projects', logRequest, middleware1, middleware2, (request, response)=> {
+// é executado em sequencia
+app.get('/projects', logRequest, (request, response)=> {
+  const {title } = request.query;
+  // console.log(title);
+  // console.log(owner);
+  const results= title 
+  ? projects.filter(p=>p.title.includes(title)) 
+  : projects;
+  
+  return response.json(results);
+});
+
+
+app.post('/projects',(request, response)=>{
+  const {title, owner} = request.body;
+  //console.log(body);
+
+  const project = {id: uuid(), title, owner};
+  projects.push(project);
+  return response.json(project);
+
+});
+
+
+app.put('/projects/:id',(request, response)=>{
+  // const params = request.params;
+  // console.log(params);
+
+  const {id} = request.params;
+  const {title, owner} = request.body;
+  //console.log(id);
+  const projectIndex = projects.findIndex(p=>p.id===id);
+  if(projectIndex<0){
+      return response.status(400).json({
+        error:'Não encontrado.'
+      });
+    }
+
+    const project ={
+      id,
+      title,
+      owner,
+    };
+    projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id',(request, response)=>{
+ const {id} = request.params;
+  
+  //console.log(id);
+  const projectIndex = projects.findIndex(p=>p.id===id);
+  if(projectIndex<0){
+      return response.status(400).json({
+        error:'Não encontrado.'
+      });
+    }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.listen(3333,()=>{
+  console.log('✈ backend started!')
+});
---
+++ desafio01/src/index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const { uuid } = require('uuidv4');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs, likes } = request.body;
+
+  const repository = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repository);
+
+  return response.json(repository);
+  
+});
+
+app.put("/repositories/:id", (request, response) => {
+    const { id } = request.params;
+    const { title, url, techs } = request.body;
+
+    const repositoryIndex = RepositoryIndex(id);
+
+    //Monta o obj
+    const repository = {
+      id,
+      title,
+      url,
+      techs,
+    }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
+});
+
+app.delete("/repositories/:id", (req, res) => {
+  const {id} = req.params;
+
+  
+   const repositoryIndex = RepositoryIndex(id);
+
+    repositories.splice(repositoryIndex, 1);
+
+    return res.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const {id} = request.params;
+  const { title, url, techs, likes } = request.body;
+
+  const repositoryIndex = RepositoryIndex(id);
+
+  let  repository =  repositories[repositoryIndex];
+  let totalLike = repository.likes;
+  
+  repository = {
+      id,
+      title, 
+      url, 
+      techs,
+      likes: ++totalLike,
+    }
+
+    repositories[repositoryIndex] = repository;
+
+    return response.json(repository);
+
+});
+
+
+function  RepositoryIndex(id) {
+   //Busca o index da lista
+    const repositoryIndex = repositories.findIndex(r=>r.id===id);
+
+    //Verifica se existe
+    if(repositoryIndex < 0){
+      return response.status(400).json({
+        error: 'Repository not found.',
+      });
+    }
+
+    return repositoryIndex;
+}
+
+app.listen(3333,()=>{
+  console.log('✈ backend Ok!')
+});
